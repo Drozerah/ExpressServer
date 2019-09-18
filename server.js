5947c40598afda463b830e1e7d83b2de9e703a54
Drozerah <drozerah@gmail.com>
2019-09-18 17:22:33 +0200

    refactor: add app start binary
    - feat: add ./bin folder
    - feat: add ./bin/www file:
    	- add shebang permission
    	- import dependencies
    	- create normalizePort()
    	- set app port
    	- listen for connection on the specified port
    - feat: npm start script now runs ./bin/www binary file

--- server.js
+++ server.js
@@ -2,14 +2,8 @@
  * Module dependencies
  */
 const express = require('express')
-/**
-* NPM modules 
-*/
-const chalk = require('chalk')
 
 const app = express()
-
-app.set('port', process.env.port || 3000)
 
 // custom 404 page
 app.use((req, res) => {
@@ -24,12 +18,6 @@ app.use((err, req, res, next) => {
     res.type('text/plain')
     res.status(500)
     res.send('500 - Server Error')
-})
-
-app.listen(app.get('port'), () => {
-    const msg = `Express started on http://localhost:${app.get('port')}
-press Ctrl-c to terminate`
-console.log(chalk.green(msg))
 })
 
 // export app
