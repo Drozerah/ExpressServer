bcabd8ba9a160a18c76ea24f01ef11a849058f23
Drozerah <drozerah@gmail.com>
2019-09-18 19:28:26 +0200

    chore: add ESLint with standard configuration

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const app = express()
 
 // custom 404 page
 app.use((req, res) => {
-    res.type('text/plain')
-    res.status(404)
-    res.send('404 - Not Found')
+  res.type('text/plain')
+  res.status(404)
+  res.send('404 - Not Found')
 })
 
 // custom 500 page
 app.use((err, req, res, next) => {
-    console.log(err.stack)
-    res.type('text/plain')
-    res.status(500)
-    res.send('500 - Server Error')
+  console.log(err.stack)
+  res.type('text/plain')
+  res.status(500)
+  res.send('500 - Server Error')
 })
 
 // export app
